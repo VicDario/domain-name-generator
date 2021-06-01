0436b8dd8f8a1ec4bcaf6e801685f79b904f2753
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-01 14:40:16 -0400

    Task finshed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "my", ""];
+  let adjs = [
+    "great",
+    "big",
+    "beautiful",
+    "necessary",
+    "safe",
+    "strange",
+    "sweet",
+    "strong",
+    "wise",
+    "young"
+  ];
+  let nouns = ["jogger", "racoon", "bank", "site"];
+  let dots = [".com", ".es", ".tv", ".cl", ".net", ".org"];
+
+  let div = document.querySelector("#domains");
+  let domain = "";
+
+  for (const pronoun of pronouns) {
+    for (const adj of adjs) {
+      for (const noun of nouns) {
+        for (const dot of dots) {
+          let insertDomain = document.createElement("P");
+          domain = `${pronoun}${adj}${noun}${dot}`;
+          insertDomain.innerHTML = domain;
+          div.appendChild(insertDomain);
+        }
+      }
+    }
+  }
 };
